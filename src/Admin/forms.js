4b8c08bd22fbc04286eff9ca78028b6815079989
MoFares7 <faresdabbas1@gmail.com>
2024-01-17 21:04:16 +0300

    upload

---
+++ src/Admin/forms.js
@@ -0,0 +1,16 @@
+
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+const Forms = () => {
+    return(
+        <Container>
+
+        </Container>
+    )
+    
+}
+export default Forms
